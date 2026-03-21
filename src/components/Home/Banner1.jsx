bb90cd01083c60037275e58c8efe8cf02e7eb7ae
Giridhar D <girigiridhar0603@gmail.com>
2026-03-21 07:18:09 +0530

    changes logo quality

--- src/components/Home/Banner1.jsx
+++ src/components/Home/Banner1.jsx
@@ -25,7 +25,7 @@ const Banner1 = () => {
           <div className="shrink-0">
             <div className="bg-white rounded-xl shadow-md p-4">
               <img
-                src="https://storage.googleapis.com/campaigners-images/Temple%20Images/hkm%20logo%20png%20-%20black%20font.jpg"
+                src="https://storage.googleapis.com/campaigners-images/Temple%20Images/hkmlogopng-blackfont.jpg"
                 alt="Hare Krishna Movement Vizag"
                 className="h-20 md:h-24 object-contain"
               />
