24316ce22680831cdc79321423f6c9340ada170c
Giridhar D <girigiridhar0603@gmail.com>
2026-03-23 12:37:38 +0530

    updated home banner

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const Banner = () => {
   return (
     <div className="w-full rounded-2xl overflow-hidden my-2.5 flex items-center justify-center">
       <img
         className="max-h-full max-w-full object-cover"
-        src="https://campaigns.iskconmangalore.org/static/media/mandir_nirman_seva_banner.4e65693c624cb9cc49cc.png"
+        src="https://storage.googleapis.com/campaigners-images/Temple%20Images/home-banner.jpg"
         alt="banner"
         loading="lazy"
       />
