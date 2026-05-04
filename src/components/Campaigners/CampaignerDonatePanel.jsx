76758ab189c502a48a2a149462c78332d56c25b1
saisivaramchalla-inevosys <saisivaram.challa@inevosys.com>
2026-05-04 21:29:05 +0530

    amount changes

--- src/components/Campaigners/CampaignerDonatePanel.jsx
+++ src/components/Campaigners/CampaignerDonatePanel.jsx
@@ -18,7 +18,7 @@ const CampaignDonatePanel = ({ details, sevas = [], sevaLoading }) => {
 
   const today = new Date();
   // const quickAmounts = [501, 1001, 2501, 5001];
-  const quickAmounts = [3016, 6016, 1116, 25116];
+  const quickAmounts = [3016, 6016, 11116, 25116];
 
   const diffDays = details?.campaginers?.campaignId?.endDate
     ? Math.max(
